4e0a8c1a9a3f7c6728111f5902155bc70b7257b1
Yennefer <wariushas@gmail.com>
2024-05-22 19:40:52 +0300

    fixed bags

--- TRAININGS/Filters/Filters.py
+++ TRAININGS/Filters/Filters.py
@@ -11,15 +11,19 @@ from scipy.ndimage import filters
 im_cv  = cv2.imread(os.path.join('.', 'logo.jpg'))
 print(im_cv)
 blur = cv2.GaussianBlur(im_cv,(5,5),0) # standart deviation
-cv2.imshow('blur_im', blur)
+# cv2.imshow('blur_im', blur)
+# cv2.waitKey(0)
 
 # with scipy
-im = np.array(Image.open('./logo.jpg')).convert('L')
-im2 = filters.gaussian_filter(im,5)
+# im = np.array(Image.open('./logo.jpg')).convert('L')
+# im2 = filters.gaussian_filter(im,5)
+
+cv2.imshow('im', im_cv[:,:,1])
+cv2.waitKey(0)
 
 
-im_new = np.zeros(im.shape)
-for i in range(3):
-    im_new[:,:,i] = filters.gaussian_filter(im[:,:,i],5)
+# im_new = np.zeros(im_cv.shape)
+# for i in range(3):
+#     im_new[:,:,i] = filters.gaussian_filter(im[:,:,i],5)
 
 
--- TRAININGS/Filters/test.py
+++ TRAININGS/Filters/test.py
@@ -5,3 +5,6 @@ import numpy as np
 
 img = cv2.imread(os.path.join('.','logo.jpg'))
 print(img)
+
+cv2.imshow('img', img)
+cv2.waitKey(0)
